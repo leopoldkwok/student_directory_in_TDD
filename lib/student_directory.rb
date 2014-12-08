d56a05adaa2788326eb20b9f32ec100d43520d62
leopoldkwok <leopoldkwok@gmail.com>
2014-12-08 13:46:07 +0000

    obtaining student data

--- lib/student_directory.rb
+++ lib/student_directory.rb
@@ -1,35 +1,52 @@
 require 'date'
 
 def print_header
-	"The students of Makers Academy\n======================================="
+		"The students of Makers Academy\n======================================="
 end
 
 def print_footer
-	"Overall, we have #{student_list.length} great students"
+		"Overall, we have #{student_list.length} great students"
 end
 
 def student_list
-	[]
+		@student_list ||=[]
+end
+
+def add(student)
+		student_list << student
 end
 
 def student_name
-	"Leo"
+		puts "What's your name?"
+		name = gets.chomp
 end
 
-def array_iteration
-	p student_list
+def student_hobby
+		puts "What's your hobby?"
+		hobby = gets.chomp
 end
 
-def ask_for_cohort
-	"What's your cohort?"
-end
-
-def input_month
-	month = gets.chomp
-	return 'You entered the wrong month name!' unless month_exists?(month)
-	month
+def student_cohort
+		puts "What's your cohort?"
+		month = gets.chomp
+		return 'You entered the wrong month name!' unless month_exists?(month)
+		month
 end
 
 def month_exists?(month)
-	Date::MONTHNAMES.include?(month.downcase.capitalize)
+		Date::MONTHNAMES.include?(month.downcase.capitalize)
+end
+
+def student_to_s(student)
+	"#{student[:name]} from the #{student[:cohort]} Cohort likes #{student[:hobby.downcase]}"
+end
+
+def list_of_students
+	student_list.map.with_index(1) do |student, index|
+		"#{index}.#{student_to_s(student)}"
+		end.join("\n")
+end
+
+def print_students
+		puts list_of_students
 end
--- spec/student_directory_spec.rb
+++ spec/student_directory_spec.rb
@@ -1,56 +1,86 @@
 require 'student_directory'
 
 describe 'Student Directory TDD Project' do 
-	context 'print header' do
+
+	context 'print information' do
 		it 'prints header here' do
-			header = "The students of Makers Academy\n======================================="
-			expect(print_header).to eq header
+				header = "The students of Makers Academy\n======================================="
+				expect(print_header).to eq header
 		end
-	end
 
-	context 'print_footer' do 
 		it 'print footer here' do
-			footer = "Overall, we have #{student_list.length} great students"
-			expect(print_footer).to eq footer
+				footer = "Overall, we have #{student_list.length} great students"
+				expect(print_footer).to eq footer
+		end
+
+		it 'asks for all information' do 
+
 		end
 	end
 
-	context 'student_name' do 
+
+	context 'student directory' do 
+
+		it "has no students" do 
+				expect(student_list).to eq []
+		end
+
+		it "adds a student" do 
+				student = {:name => "Leopold", :cohort => :August, :hobby => "Table Tennis"}
+				add(student)
+				expect(student_list).to eq [student]
+		end
+
+		it 'takes a student' do 
+				student = {name: "Leopold", cohort: :August, :hobby => "Table Tennis"}
+				expect(student_to_s(student)).to eq 'Leopold from the August Cohort likes Table Tennis'
+		end
+
 		it "print each name" do
-			expect(student_name).to eq "Leo"
+				%w{Leopold Jerome Ben}.each do |name|
+					add({name: name, cohort: :August, hobby: "Table Tennis"})
+			end
+			expect(list_of_students).to eq("1.Leopold from the August Cohort likes Table Tennis\n2.Jerome from the August Cohort likes Table Tennis\n3.Ben from the August Cohort likes Table Tennis") 
+			print_students
 		end
 	end
+	
+	context 'user for input' do 
 
-	context 'array_iteration' do 
-		it "print each name" do 
-			student_list.each do |student_list| end
-			expect(array_iteration).to eq student_list
+		it "ask user for student name" do 
+				name = "Leopold\n"
+				expect(self).to receive(:puts).with("What's your name?")
+				expect(self).to receive(:gets).and_return(name)
+				expect(student_name).to eq 'Leopold'
 		end
-	end
 
-	context 'student_cohort' do 
-		it "asks student for cohort" do 
-			expect(ask_for_cohort).to eq "What's your cohort?"
+		it "ask user for hobby" do 
+				hobby = "Table Tennis\n"
+				expect(self).to receive(:puts).with("What's your hobby?")
+				expect(self).to receive(:gets).and_return(hobby)
+				expect(student_hobby).to eq 'Table Tennis'
 		end
 
 		it 'inputs a cohort' do 
-			month = "August\n"
-			expect(self).to receive(:gets).and_return(month)
-			expect(input_month).to eq "August"
+				month = "August\n"
+				expect(self).to receive(:puts).with("What's your cohort?")
+				expect(self).to receive(:gets).and_return(month)
+				expect(student_cohort).to eq "August"
 		end
 
 		it 'knows that August is a month' do
-			expect(month_exists?('august')).to be true
+				expect(month_exists?('august')).to be true
 		end
 
 		it 'knows that Christmas is not a month' do 
-			expect(month_exists?('Christmas')).not_to be true
+				expect(month_exists?('Christmas')).not_to be true
 		end
+
 
 		it 'will not allow us to input the wrong month' do 
 			month = "yikes\n"
 			allow(self).to receive(:gets).and_return(month)
-			expect(input_month).to eq 'You entered the wrong month name!'
+			expect(student_cohort).to eq 'You entered the wrong month name!'
 		end
 	end
 end
